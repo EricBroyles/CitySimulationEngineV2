d2178aa6c3b2f8661968ac80d0037918c6991603
EricBroyles <eric.christopher.broyles@gmail.com>
2026-01-07 22:21:48 -0500

    added dev.txt with sudocode for simulatio

--- godot_project/experiments/example.gd
+++ godot_project/experiments/example.gd
@@ -1,8 +1,16 @@
 extends Node
 
+@onready var sprite = %Sprite2D
 
 func _ready() -> void:
 	var example := ExampleClass.new()
 	example.print_type(example)
-	var thing := ExpModifyImgUpdateTex.new()
+	var thing: ExpModifyImgUpdateTex = ExpModifyImgUpdateTex.new()
 	print(thing)
+	var img: Image = Image.create_empty(100,100,false, Image.FORMAT_RGBA8)
+	var tex: ImageTexture = ImageTexture.create_from_image(img)
+	thing.make_stripes(img, tex)
+	sprite.texture = tex
+	
+	
+	
